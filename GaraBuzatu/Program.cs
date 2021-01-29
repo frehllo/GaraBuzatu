a6c57f8e48dabf09023441b01add668914b77327
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-29 10:32:00 +0100

    Creazione e Completamento

--- GaraBuzatu/Program.cs
+++ GaraBuzatu/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace GaraBuzatu
 {
@@ -6,43 +11,27 @@ namespace GaraBuzatu
     {
         static void Main(string[] args)
         {
-            using System;
-            using System.Collections.Generic;
-            using System.Linq;
-            using System.Text;
-            using System.Threading.Tasks;
-            using System.Threading;
-
-namespace Galeotti_EsercizioGara
-    {
-        class Program
-        {
-            static void Main(string[] args)
+            Console.Write("Numero partecipanti : ");
+            int p = int.Parse(Console.ReadLine());
+            List<string> partecipanti = new List<string>();
+            for (int i = 1; i <= p; i++)
             {
-                Console.Write("Quanti sono i partecipanti? ");
-                int p = int.Parse(Console.ReadLine());
-                List<string> partecipanti = new List<string>();
-                for (int i = 1; i <= p; i++)
-                {
-                    Console.WriteLine("Nome del partecipante: ");
-                    string nome = Console.ReadLine();
-                    partecipanti.Add(nome);
-                    Thread t = new Thread(() => Corri(nome));
-                    t.Start();
-                }
-                Console.ReadLine();
+                Console.WriteLine("Nome partecipante : ");
+                string nome = Console.ReadLine();
+                partecipanti.Add(nome);
+                Thread t = new Thread(() => Corri(nome));
+                t.Start();
             }
-
-            private static void Corri(string nome)
-            {
-                for (int i = 0; i < 99; i++)
-                {
-                    Console.WriteLine($"Il partecipante {nome} è al km {i + 1}.");
-                }
-                Console.WriteLine($"Il partecipante {nome} è arrivato al km 100 e ha terminato la corsa.");
-            }
+            Console.ReadLine();
         }
-    }
-}
+
+        private static void Corri(string nome)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                Console.WriteLine($"{nome} è al km {i + 1}.");
+            }
+            Console.WriteLine($"{nome} è arrivato al km 100 e ha terminato la gara.");
+        }
     }
 }
